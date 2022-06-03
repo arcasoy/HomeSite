3da317d4c308ec7b6bc06971b48c6f7186e16832
arcasoy <arcasoy1@gmail.com>
2022-06-02 23:18:44 -0400

    adding plaid backend

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
+var plaidRouter = require('./routes/plaid');
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
+app.use('/plaid', plaidRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
