2b0c269e9887b05973f66281505dbda9c0d73a76
arcasoy <arcasoy1@gmail.com>
2022-06-07 00:06:52 -0400

    implemented Link authorization flow for Plaid

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Full stack project for personal & home uses
 - Budgeting
 - Home Automation
+
+ToDo:
+- replace Moment module with something not in legacy
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import SimplePlaidLink from './Components/SimplePlaidLink/SimplePlaidLink'
 
 class App extends Component{
   constructor(props) {
@@ -29,6 +30,7 @@ class App extends Component{
           <p className ="App-intro">
             {this.state.apiResponse}
           </p>
+          <SimplePlaidLink></SimplePlaidLink>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -42,28 +44,5 @@ class App extends Component{
     );
   }
 }
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 
 export default App;
